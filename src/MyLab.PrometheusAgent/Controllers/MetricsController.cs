90c2277def1a64cb81b41e08ed15f3a8626ddb07
Andrey O <ozzy.ext@gmail.com>
2021-06-23 22:53:07 +0300

    Prepare project

---
+++ src/MyLab.PrometheusAgent/Controllers/MetricsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyLab.PrometheusAgent.Controllers
+{
+    [ApiController]
+    [Route("metrics")]
+    public class MetricsController : ControllerBase
+    {
+        private readonly ILogger<MetricsController> _logger;
+
+        public MetricsController(ILogger<MetricsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
--- src/MyLab.PrometheusAgent/Startup.cs
+++ src/MyLab.PrometheusAgent/Startup.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MyLab.WebErrors;
 
 namespace MyLab.PrometheusAgent
 {
@@ -24,8 +20,8 @@ namespace MyLab.PrometheusAgent
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddLogging(c => c.AddConsole());
+            services.AddControllers(c => c.AddExceptionProcessing());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
