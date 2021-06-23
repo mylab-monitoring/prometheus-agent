0fc3eac0d6a12148a3e29c227a764d5a32f13a47
Andrey O <ozzy.ext@gmail.com>
2021-06-23 23:30:57 +0300

    Add config loading and tests

---
+++ src/MyLab.PrometheusAgent/Tools/ScrapeConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Serialization;
+
+namespace MyLab.PrometheusAgent.Tools
+{
+    public class ScrapeConfig
+    {
+        static readonly IDeserializer Deserializer =
+            new DeserializerBuilder()
+                .IgnoreUnmatchedProperties()
+                .Build();
+
+        [YamlMember(Alias = "scrape_configs")]
+        public ScrapeConfigItem[] Items { get; set; }
+        
+        public static ScrapeConfig Parse(string yaml)
+        {
+            return Deserializer.Deserialize<ScrapeConfig>(yaml);
+        }
+    }
+
+    public class ScrapeConfigItem
+    {
+        [YamlMember(Alias = "job_name")]
+        public string JobName { get; set; }
+
+        [YamlMember(Alias = "static_configs")]
+        public ScrapeStaticConfig[] StaticConfigs { get; set; }
+    }
+
+    public class ScrapeStaticConfig
+    {
+        [YamlMember(Alias = "targets")]
+        public string[] Targets { get; set; }
+
+        [YamlMember(Alias = "labels")]
+        public Dictionary<string, string> Labels { get; set; }
+    }
+}
