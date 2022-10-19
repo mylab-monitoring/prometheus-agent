dd3529284a031580ea67d011ba1245b5e4589189
Andrey O <ozzy.ext@gmail.com>
2022-10-19 21:46:48 +0300

    Add tester

---
+++ src/MyLab.PrometheusAgent.Tester/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyLab.Log;
+using MyLab.Log.Serializing;
+using MyLab.PrometheusAgent;
+using YamlDotNet.Serialization;
+
+if (!File.Exists("metrics.txt"))
+{
+    Console.WriteLine("Error: File 'metrics.txt' not found!");
+}
+
+try
+{
+    var metricsStr = await File.ReadAllTextAsync("metrics.txt");
+
+    using var reader = new StringReader(metricsStr);
+
+    await MetricModel.ReadAsync(reader);
+}
+catch (Exception e)
+{
+    var dto = ExceptionDto.Create(e);
+
+    var serializer = new SerializerBuilder().Build();
+    var yaml = serializer.Serialize(dto);
+
+    Console.WriteLine(yaml);
+}
+
