fedb26f04a14995e7bd98a043211be3f536383e9
Andrey O <ozzy.ext@gmail.com>
2023-01-26 17:00:57 +0300

    Add new service label prefix

--- src/MyLab.PrometheusAgent/Tools/ServiceLabelExcludeLogic.cs
+++ src/MyLab.PrometheusAgent/Tools/ServiceLabelExcludeLogic.cs
@@ -14,6 +14,7 @@ namespace MyLab.PrometheusAgent.Tools
         private static readonly string[] ServiceLabelsStartWith = new[]
         {
             "com.docker.compose.",
+            "org.opencontainers.",
             "desktop.docker."
         };
         
